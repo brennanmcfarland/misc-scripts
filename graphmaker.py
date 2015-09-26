d076687fdce2a88f98edd79b8e7b7f85a02f1578
Brennan McFarland <4brothersnosisters@gmail.com>
2015-09-26 17:15:52 -0400

    started working on fractal function

--- graphmaker.py
+++ graphmaker.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import math
 
-def graphcurve(xmin,xmax,xres,function, *args):
+def graphfunction(xmin,xmax,xres,function, *args):
     x,y = [],[]
     i=0
     while xmin+i*xres<=xmax:
@@ -10,11 +10,26 @@ def graphcurve(xmin,xmax,xres,function, *args):
         i+=1
     return [x,y]
 
+def koch_snowflake(iterations):
+    x,y = [],[]
+    for i in range(0,iterations):
+        iterationpoints = iterate_koch_snowflake(0.0,1.0)
+        x.insert(x.size/(2*math.pow(i)), iterationpoints[0][0])
+    return [x,y]
+
+def iterate_koch_snowflake(a,b):
+    "a and b are points; [x,y]"
+    newa = [(a[0]+(b[0]-a[0])/3),((b[1]-a[1])/(b[0]-a[1])(b[0]-a[0])/3+a[1])]
+    newb = [(a[0]+2*(b[0]-a[0])/3),(2*(b[1]-a[1])/(b[0]-a[1])(b[0]-a[0])/3+a[1])]
+    newapex = [newb[0]-newa[0],newb[1]-newa[1]]
+    return [newa,newapex,newb]
+
 def line_slope_intercept(x,m,b):
     return m*x+b
+
 def line_point_slope(x,m,x1,y1):
     return m*(x-x1)+y1
 
-points = graphcurve(0,10,.01,math.tan)
+points = graphfunction(0,10,.01,math.tan)
 plt.plot(points[0],points[1])
 plt.show()
